724c48a7c4adffe5ef0c5f7a5d9d0431b657e034
wesly111 <tsaiwesiey2005@gmail.com>
2025-12-08 14:56:10 +0800

    add file

---
+++ mc.cpp
@@ -0,0 +1,181 @@
+///////////////////////////////////////////////////////////////////////////
+// C++ code generated with wxFormBuilder (version 4.2.1-0-g80c4cb6)
+// http://www.wxformbuilder.org/
+//
+// PLEASE DO *NOT* EDIT THIS FILE!
+///////////////////////////////////////////////////////////////////////////
+
+#include "MyProjectBase.h"
+
+///////////////////////////////////////////////////////////////////////////
+
+MyPanel1::MyPanel1(wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size, long style, const wxString& name) : wxPanel(parent, id, pos, size, style, name)
+{
+	wxGridSizer* gSizer1;
+	gSizer1 = new wxGridSizer(8, 3, 0, 0);
+
+	m_bpButton1 = new wxBitmapButton(this, wxID_ANY, wxNullBitmap, wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW | 0);
+
+	m_bpButton1->SetBitmap(wxBitmap(wxT("2025-12-06_001327.png"), wxBITMAP_TYPE_ANY));
+	m_bpButton1->SetForegroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_INFOTEXT));
+
+	gSizer1->Add(m_bpButton1, 0, wxALL | wxEXPAND, 5);
+
+	m_bpButton2 = new wxBitmapButton(this, wxID_ANY, wxNullBitmap, wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW | 0);
+
+	m_bpButton2->SetBitmap(wxBitmap(wxT("2025-12-06_001331.png"), wxBITMAP_TYPE_ANY));
+	m_bpButton2->SetForegroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_INFOTEXT));
+
+	gSizer1->Add(m_bpButton2, 0, wxALL | wxEXPAND, 5);
+
+	m_bpButton3 = new wxBitmapButton(this, wxID_ANY, wxNullBitmap, wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW | 0);
+
+	m_bpButton3->SetBitmap(wxBitmap(wxT("2025-12-06_001350.png"), wxBITMAP_TYPE_ANY));
+	m_bpButton3->SetForegroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_INFOTEXT));
+
+	gSizer1->Add(m_bpButton3, 0, wxALL | wxEXPAND, 5);
+
+	m_bpButton4 = new wxBitmapButton(this, wxID_ANY, wxNullBitmap, wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW | 0);
+
+	m_bpButton4->SetBitmap(wxBitmap(wxT("2025-12-06_001355.png"), wxBITMAP_TYPE_ANY));
+	m_bpButton4->SetForegroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_INFOTEXT));
+
+	gSizer1->Add(m_bpButton4, 0, wxALL | wxEXPAND, 5);
+
+	m_bpButton5 = new wxBitmapButton(this, wxID_ANY, wxNullBitmap, wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW | 0);
+
+	m_bpButton5->SetBitmap(wxBitmap(wxT("2025-12-06_001358.png"), wxBITMAP_TYPE_ANY));
+	m_bpButton5->SetForegroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_INFOTEXT));
+
+	gSizer1->Add(m_bpButton5, 0, wxALL | wxEXPAND, 5);
+
+	m_bpButton6 = new wxBitmapButton(this, wxID_ANY, wxNullBitmap, wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW | 0);
+
+	m_bpButton6->SetBitmap(wxBitmap(wxT("2025-12-06_001402.png"), wxBITMAP_TYPE_ANY));
+	m_bpButton6->SetForegroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_INFOTEXT));
+
+	gSizer1->Add(m_bpButton6, 0, wxALL | wxEXPAND, 5);
+
+	m_bpButton7 = new wxBitmapButton(this, wxID_ANY, wxNullBitmap, wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW | 0);
+
+	m_bpButton7->SetBitmap(wxBitmap(wxT("2025-12-06_001406.png"), wxBITMAP_TYPE_ANY));
+	m_bpButton7->SetForegroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_INFOTEXT));
+
+	gSizer1->Add(m_bpButton7, 0, wxALL | wxEXPAND, 5);
+
+	m_bpButton8 = new wxBitmapButton(this, wxID_ANY, wxNullBitmap, wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW | 0);
+
+	m_bpButton8->SetBitmap(wxBitmap(wxT("2025-12-06_001409.png"), wxBITMAP_TYPE_ANY));
+	m_bpButton8->SetForegroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_INFOTEXT));
+
+	gSizer1->Add(m_bpButton8, 0, wxALL | wxEXPAND, 5);
+
+	m_bpButton9 = new wxBitmapButton(this, wxID_ANY, wxNullBitmap, wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW | 0);
+
+	m_bpButton9->SetBitmap(wxBitmap(wxT("2025-12-06_001413.png"), wxBITMAP_TYPE_ANY));
+	m_bpButton9->SetForegroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_INFOTEXT));
+
+	gSizer1->Add(m_bpButton9, 0, wxALL | wxEXPAND, 5);
+
+	m_bpButton10 = new wxBitmapButton(this, wxID_ANY, wxNullBitmap, wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW | 0);
+
+	m_bpButton10->SetBitmap(wxBitmap(wxT("2025-12-06_001413.png"), wxBITMAP_TYPE_ANY));
+	m_bpButton10->SetForegroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_INFOTEXT));
+
+	gSizer1->Add(m_bpButton10, 0, wxALL | wxEXPAND, 5);
+
+	m_bpButton11 = new wxBitmapButton(this, wxID_ANY, wxNullBitmap, wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW | 0);
+
+	m_bpButton11->SetBitmap(wxBitmap(wxT("2025-12-06_001423.png"), wxBITMAP_TYPE_ANY));
+	m_bpButton11->SetForegroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_INFOTEXT));
+
+	gSizer1->Add(m_bpButton11, 0, wxALL | wxEXPAND, 5);
+
+	m_bpButton12 = new wxBitmapButton(this, wxID_ANY, wxNullBitmap, wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW | 0);
+
+	m_bpButton12->SetBitmap(wxBitmap(wxT("2025-12-06_001417.png"), wxBITMAP_TYPE_ANY));
+	m_bpButton12->SetForegroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_INFOTEXT));
+
+	gSizer1->Add(m_bpButton12, 0, wxALL | wxEXPAND, 5);
+
+	m_bpButton13 = new wxBitmapButton(this, wxID_ANY, wxNullBitmap, wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW | 0);
+
+	m_bpButton13->SetBitmap(wxBitmap(wxT("2025-12-06_001426.png"), wxBITMAP_TYPE_ANY));
+	m_bpButton13->SetForegroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_INFOTEXT));
+
+	gSizer1->Add(m_bpButton13, 0, wxALL | wxEXPAND, 5);
+
+	m_bpButton14 = new wxBitmapButton(this, wxID_ANY, wxNullBitmap, wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW | 0);
+
+	m_bpButton14->SetBitmap(wxBitmap(wxT("2025-12-06_001429.png"), wxBITMAP_TYPE_ANY));
+	m_bpButton14->SetForegroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_INFOTEXT));
+
+	gSizer1->Add(m_bpButton14, 0, wxALL | wxEXPAND, 5);
+
+	m_bpButton15 = new wxBitmapButton(this, wxID_ANY, wxNullBitmap, wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW | 0);
+
+	m_bpButton15->SetBitmap(wxBitmap(wxT("2025-12-06_001432.png"), wxBITMAP_TYPE_ANY));
+	m_bpButton15->SetForegroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_INFOTEXT));
+
+	gSizer1->Add(m_bpButton15, 0, wxALL | wxEXPAND, 5);
+
+	m_bpButton16 = new wxBitmapButton(this, wxID_ANY, wxNullBitmap, wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW | 0);
+
+	m_bpButton16->SetBitmap(wxBitmap(wxT("2025-12-06_001437.png"), wxBITMAP_TYPE_ANY));
+	m_bpButton16->SetForegroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_INFOTEXT));
+
+	gSizer1->Add(m_bpButton16, 0, wxALL | wxEXPAND, 5);
+
+	m_bpButton17 = new wxBitmapButton(this, wxID_ANY, wxNullBitmap, wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW | 0);
+
+	m_bpButton17->SetBitmap(wxBitmap(wxT("2025-12-06_001441.png"), wxBITMAP_TYPE_ANY));
+	m_bpButton17->SetForegroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_INFOTEXT));
+
+	gSizer1->Add(m_bpButton17, 0, wxALL | wxEXPAND, 5);
+
+	m_bpButton18 = new wxBitmapButton(this, wxID_ANY, wxNullBitmap, wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW | 0);
+
+	m_bpButton18->SetBitmap(wxBitmap(wxT("2025-12-06_001447.png"), wxBITMAP_TYPE_ANY));
+	m_bpButton18->SetForegroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_INFOTEXT));
+
+	gSizer1->Add(m_bpButton18, 0, wxALL | wxEXPAND, 5);
+
+	m_bpButton19 = new wxBitmapButton(this, wxID_ANY, wxNullBitmap, wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW | 0);
+
+	m_bpButton19->SetBitmap(wxBitmap(wxT("2025-12-06_001450.png"), wxBITMAP_TYPE_ANY));
+	m_bpButton19->SetForegroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_INFOTEXT));
+
+	gSizer1->Add(m_bpButton19, 0, wxALL | wxEXPAND, 5);
+
+	m_bpButton20 = new wxBitmapButton(this, wxID_ANY, wxNullBitmap, wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW | 0);
+
+	m_bpButton20->SetBitmap(wxBitmap(wxT("2025-12-06_001453.png"), wxBITMAP_TYPE_ANY));
+	m_bpButton20->SetForegroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_INFOTEXT));
+
+	gSizer1->Add(m_bpButton20, 0, wxALL | wxEXPAND, 5);
+
+	m_bpButton21 = new wxBitmapButton(this, wxID_ANY, wxNullBitmap, wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW | 0);
+
+	m_bpButton21->SetBitmap(wxBitmap(wxT("2025-12-06_001455.png"), wxBITMAP_TYPE_ANY));
+	m_bpButton21->SetForegroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_INFOTEXT));
+
+	gSizer1->Add(m_bpButton21, 0, wxALL | wxEXPAND, 5);
+
+	m_button1 = new wxButton(this, wxID_ANY, _("結帳"), wxDefaultPosition, wxDefaultSize, 0);
+	m_button1->SetBackgroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_ACTIVEBORDER));
+
+	gSizer1->Add(m_button1, 0, wxALIGN_BOTTOM | wxALL | wxSHAPED, 5);
+
+	m_textCtrl1 = new wxTextCtrl(this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0);
+	m_textCtrl1->SetFont(wxFont(20, wxFONTFAMILY_ROMAN, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL, false, wxT("新細明體")));
+
+	gSizer1->Add(m_textCtrl1, 0, wxALIGN_BOTTOM | wxALL | wxSHAPED, 5);
+
+
+	this->SetSizer(gSizer1);
+	this->Layout();
+}
+
+MyPanel1::~MyPanel1()
+{
+}
---
+++ mc.h
@@ -0,0 +1,69 @@
+#pragma once
+///////////////////////////////////////////////////////////////////////////
+// C++ code generated with wxFormBuilder (version 4.2.1-0-g80c4cb6)
+// http://www.wxformbuilder.org/
+//
+// PLEASE DO *NOT* EDIT THIS FILE!
+///////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include <wx/artprov.h>
+#include <wx/xrc/xmlres.h>
+#include <wx/intl.h>
+#include <wx/bmpbuttn.h>
+#include <wx/string.h>
+#include <wx/bitmap.h>
+#include <wx/image.h>
+#include <wx/icon.h>
+#include <wx/gdicmn.h>
+#include <wx/font.h>
+#include <wx/colour.h>
+#include <wx/settings.h>
+#include <wx/button.h>
+#include <wx/textctrl.h>
+#include <wx/sizer.h>
+#include <wx/panel.h>
+
+///////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+/// Class MyPanel1
+///////////////////////////////////////////////////////////////////////////////
+class MyPanel1 : public wxPanel
+{
+private:
+
+protected:
+	wxBitmapButton* m_bpButton1;
+	wxBitmapButton* m_bpButton2;
+	wxBitmapButton* m_bpButton3;
+	wxBitmapButton* m_bpButton4;
+	wxBitmapButton* m_bpButton5;
+	wxBitmapButton* m_bpButton6;
+	wxBitmapButton* m_bpButton7;
+	wxBitmapButton* m_bpButton8;
+	wxBitmapButton* m_bpButton9;
+	wxBitmapButton* m_bpButton10;
+	wxBitmapButton* m_bpButton11;
+	wxBitmapButton* m_bpButton12;
+	wxBitmapButton* m_bpButton13;
+	wxBitmapButton* m_bpButton14;
+	wxBitmapButton* m_bpButton15;
+	wxBitmapButton* m_bpButton16;
+	wxBitmapButton* m_bpButton17;
+	wxBitmapButton* m_bpButton18;
+	wxBitmapButton* m_bpButton19;
+	wxBitmapButton* m_bpButton20;
+	wxBitmapButton* m_bpButton21;
+	wxButton* m_button1;
+	wxTextCtrl* m_textCtrl1;
+
+public:
+
+	MyPanel1(wxWindow* parent, wxWindowID id = wxID_ANY, const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize(900, 2400), long style = wxTAB_TRAVERSAL, const wxString& name = wxEmptyString);
+
+	~MyPanel1();
+
+};
+
